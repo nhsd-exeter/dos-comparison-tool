9973aca7cd7559c40621c43f5ab5ed4e8d8a5b83
Jack Plowman <jack.plowman1@nhs.net>
2022-11-24 12:31:43 +0000

    Remove unused typescript files

--- application/ui/src/main.tsx
+++ application/ui/src/main.tsx
@@ -1,11 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
-import { getPageTitle, setPageTitle } from "./utils/pageTitles";
-import { pageMeta, siteTitle } from "./interfaces/pageMeta";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import Application from "./components/application";
-
-setPageTitle(getPageTitle(pageMeta, window.location.pathname, siteTitle));
 
 const DoSComparisonTool = () => (
 	<Provider store={store}>
