b062a8d8e7dd1d690dd30007d757303601e326ed
Jack Plowman <jack.plowman1@nhs.net>
2022-12-02 09:43:27 +0000

    Fix s3 tfsec permissions

--- infrastructure/stacks/development-and-deployment-tools/s3.tf
+++ infrastructure/stacks/development-and-deployment-tools/s3.tf
@@ -1,7 +1,24 @@
+#tfsec:ignore:aws-s3-encryption-customer-key
+#tfsec:ignore:aws-s3-enable-bucket-logging
 module "development_pipeline_artefact_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "3.6.0"
   bucket  = var.development_pipeline_bucket
+
+  # Allow deletion of non-empty bucket
+  force_destroy = true
+
+  versioning = {
+    enabled = true
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 
   // S3 bucket-level Public Access Block configuration
   block_public_acls                     = true
