463d887df63708611330698b7ae5306cdf1cd89c
Jack Plowman <jack.plowman1@nhs.net>
2022-12-07 14:18:06 +0000

    Get login details from secrets and use them to sign into application

--- test/end_to_end/conftest.py
+++ test/end_to_end/conftest.py
@@ -4,11 +4,11 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 from .utils.drivers.chrome_driver import close_driver, get_driver
-from .utils.pages.homepage import HomePage
+from .utils.pages.home_page import HomePage
 
 
 def pytest_bdd_before_scenario():
-    """Create a driver before each scenario."""
+    """Create a driver before each scenario then navigate to the homepage."""
     driver = get_driver()
     HomePage().navigate_to_page()
     WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, "DoSComparisonToolHeader")))
---
+++ test/end_to_end/utils/pages/login_page.py
@@ -0,0 +1,41 @@
+from .page import Page
+from ..drivers.chrome_driver import get_driver
+from boto3 import client
+from os import getenv
+from json import loads
+
+
+class LoginPage(Page):
+    """Login Page."""
+
+    url_subdirectory = "/login"
+    page_number = 2
+
+    def login(self):
+        """Login to the application. User will be left on menu page."""
+        assert self.url_subdirectory in get_driver().current_url
+        username, password = self.get_username_and_password()
+        self.input_username(username)
+        self.input_password(password)
+        self.navigate_to_next_page()
+
+    def input_username(self, username: str):
+        """Input the username."""
+        get_driver().find_element("id", "authUsernameInput").send_keys(username)
+
+    def input_password(self, password: str):
+        """Input the password."""
+        get_driver().find_element("id", "authPasswordInput").send_keys(password)
+
+    def get_username_and_password(self) -> tuple[str, str]:
+        """Get the username and password from secrets manager.
+
+        Returns:
+            tuple[str, str]: Username and password in that order.
+        """
+        secret_name = getenv("COGNITO_SECRETS_NAME")
+        secret = client("secretsmanager").get_secret_value(SecretId=secret_name)
+        secret_value = loads(secret["SecretString"])
+        username = secret_value[getenv("COGNITO_SECRETS_ADMIN_USERNAME_KEY")]
+        password = secret_value[getenv("COGNITO_SECRETS_ADMIN_PASSWORD_KEY")]
+        return username, password
