4b53973baea449aaf2621763b1b4597fc499da67
Jack Plowman <jack.plowman1@nhs.net>
2023-04-03 13:51:17 +0100

    Fix checkov warnings

--- infrastructure/stacks/application/api-gateway-search-path-cors.tf
+++ infrastructure/stacks/application/api-gateway-search-path-cors.tf
@@ -1,10 +1,3 @@
-resource "aws_api_gateway_method" "search_cors_method" {
-  rest_api_id   = aws_api_gateway_rest_api.dos_comparison_tool_api_gateway.id
-  resource_id   = aws_api_gateway_resource.search_path.id
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
-
 resource "aws_api_gateway_integration" "search_cors_integration" {
   rest_api_id          = aws_api_gateway_rest_api.dos_comparison_tool_api_gateway.id
   resource_id          = aws_api_gateway_resource.search_path.id
@@ -36,43 +29,10 @@ resource "aws_api_gateway_integration_response" "search_cors_integration_respons
   ]
 }
 
-resource "aws_api_gateway_method_response" "search_cors_method_response" {
-  rest_api_id = aws_api_gateway_rest_api.dos_comparison_tool_api_gateway.id
-  resource_id = aws_api_gateway_resource.search_path.id
-  http_method = aws_api_gateway_method.search_cors_method.http_method
-  status_code = 200
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
-
-  response_models = {
-    "application/json" = "Empty"
-  }
-
-  depends_on = [
-    aws_api_gateway_method.search_cors_method,
-  ]
-}
-
-resource "aws_api_gateway_gateway_response" "default_4xx_gateway_response" {
+resource "aws_api_gateway_method" "search_cors_method" {
+  #checkov:skip=CKV2_AWS_53:Need to validate the request body once schema is defined
   rest_api_id   = aws_api_gateway_rest_api.dos_comparison_tool_api_gateway.id
-  response_type = "DEFAULT_4XX"
-  response_parameters = {
-    "gatewayresponse.header.Access-Control-Allow-Headers" = "'Origin,Content-Type,Accept,Authorization'"
-    "gatewayresponse.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
-    "gatewayresponse.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-}
-
-resource "aws_api_gateway_gateway_response" "default_5xx_gateway_response" {
-  rest_api_id   = aws_api_gateway_rest_api.dos_comparison_tool_api_gateway.id
-  response_type = "DEFAULT_5XX"
-  response_parameters = {
-    "gatewayresponse.header.Access-Control-Allow-Headers" = "'Origin,Content-Type,Accept,Authorization'"
-    "gatewayresponse.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
-    "gatewayresponse.header.Access-Control-Allow-Origin"  = "'*'"
-  }
+  resource_id   = aws_api_gateway_resource.search_path.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
 }
--- infrastructure/stacks/application/api-gateway.tf
+++ infrastructure/stacks/application/api-gateway.tf
@@ -81,3 +81,44 @@ resource "aws_cloudwatch_log_group" "di_endpoint_access_logs" {
   name              = "/aws/api-gateway/${var.api_gateway_name}"
   retention_in_days = 30
 }
+
+resource "aws_api_gateway_method_response" "search_cors_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.dos_comparison_tool_api_gateway.id
+  resource_id = aws_api_gateway_resource.search_path.id
+  http_method = aws_api_gateway_method.search_cors_method.http_method
+  status_code = 200
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  depends_on = [
+    aws_api_gateway_method.search_cors_method,
+  ]
+}
+
+resource "aws_api_gateway_gateway_response" "default_4xx_gateway_response" {
+  rest_api_id   = aws_api_gateway_rest_api.dos_comparison_tool_api_gateway.id
+  response_type = "DEFAULT_4XX"
+  response_parameters = {
+    "gatewayresponse.header.Access-Control-Allow-Headers" = "'Origin,Content-Type,Accept,Authorization'"
+    "gatewayresponse.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
+    "gatewayresponse.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
+resource "aws_api_gateway_gateway_response" "default_5xx_gateway_response" {
+  rest_api_id   = aws_api_gateway_rest_api.dos_comparison_tool_api_gateway.id
+  response_type = "DEFAULT_5XX"
+  response_parameters = {
+    "gatewayresponse.header.Access-Control-Allow-Headers" = "'Origin,Content-Type,Accept,Authorization'"
+    "gatewayresponse.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
+    "gatewayresponse.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
