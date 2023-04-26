a7d5c9aa96cfbf6100034d4852713de74ddede2a
Jack Plowman <jack.plowman1@nhs.net>
2023-04-26 19:38:36 +0100

    Fix TFSec warning

--- infrastructure/stacks/application/cloudwatch-logs.tf
+++ infrastructure/stacks/application/cloudwatch-logs.tf
@@ -1,3 +1,4 @@
+#tfsec:ignore:aws-cloudwatch-log-group-customer-key
 resource "aws_cloudwatch_log_group" "api_gateway_waf_logs" {
   name              = var.api_gateway_waf_logs_name
   retention_in_days = "30"
