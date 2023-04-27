741622f6001e5aec2db270aa5409c3e4e8bdefb9
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2023-04-27 12:29:48 +0100

    DOSCT-67 Reduce Python Code Duplication (#148)
    # Task Branch Pull Request
    
    ## Description of Changes
    
    This PR reduces the duplication of python code in the application
    folder.

--- application/search/tests/ccs_comparison_search/test_check_capacity_summary_search.py
+++ application/search/tests/ccs_comparison_search/test_check_capacity_summary_search.py
@@ -28,33 +28,39 @@ class TestCheckCapacitySummarySearch:
     expected_search_distance = 20
     expected_version = 1.5
 
+    default_ccs_search = CheckCapacitySummarySearch(
+        age=age,
+        age_format=age_format,
+        disposition=disposition,
+        symptom_group=symptom_group,
+        symptom_discriminator_list=symptom_discriminator_list,
+        gender=gender,
+        role=role,
+        postcode=postcode,
+        search_environment=search_environment,
+        search_date_time=search_date_time,
+    )
+
     def test__init__(self) -> None:
         """Test the __init__ method."""
-        # Act
-        ccs_search = CheckCapacitySummarySearch(
-            age=self.age,
-            age_format=self.age_format,
-            disposition=self.disposition,
-            symptom_group=self.symptom_group,
-            symptom_discriminator_list=self.symptom_discriminator_list,
-            gender=self.gender,
-            role=self.role,
-            postcode=self.postcode,
-            search_environment=self.search_environment,
-            search_date_time=self.search_date_time,
-        )
         # Assert
-        assert ccs_search.age == self.age, "Age not set correctly"
-        assert ccs_search.age_format == self.age_format, "Age format not set correctly"
-        assert ccs_search.disposition == self.disposition, "Disposition not set correctly"
-        assert ccs_search.symptom_group == self.symptom_group, "Symptom group not set correctly"
+        assert self.default_ccs_search.age == self.age, "Age not set correctly"
+        assert self.default_ccs_search.age_format == self.age_format, "Age format not set correctly"
+        assert self.default_ccs_search.disposition == self.disposition, "Disposition not set correctly"
+        assert self.default_ccs_search.symptom_group == self.symptom_group, "Symptom group not set correctly"
         assert (
-            ccs_search.symptom_discriminator_list == self.symptom_discriminator_list
+            self.default_ccs_search.symptom_discriminator_list == self.symptom_discriminator_list
         ), "Symptom discriminator list not set correctly"
-        assert ccs_search.gender == self.gender, "Gender not set correctly"
-        assert ccs_search.postcode == self.postcode, "Postcode not set correctly"
-        assert ccs_search.search_environment == self.search_environment, "Search environment not set correctly"
-        assert ccs_search.search_distance == self.expected_search_distance, "Search distance not set correctly"
+        assert self.default_ccs_search.gender == self.gender, "Gender not set correctly"
+        assert self.default_ccs_search.postcode == self.postcode, "Postcode not set correctly"
+        assert (
+            self.default_ccs_search.search_environment == self.search_environment
+        ), "Search environment not set correctly"
+        assert (
+            self.default_ccs_search.search_distance == self.expected_search_distance
+        ), "Search distance not set correctly"
+        assert self.default_ccs_search.version == self.expected_version, "Version not set correctly"
+        assert self.default_ccs_search.search_date_time == self.search_date_time, "Search date time not set correctly"
 
     @patch(f"{FILE_PATH}.logger")
     @patch(f"{FILE_PATH}.CheckCapacitySummarySearch._parse_xml_response")
@@ -79,21 +85,9 @@ class TestCheckCapacitySummarySearch:
         mock__get_non_prod_username_and_password.return_value = (username, password)
         mock__build_request_data.return_value = return_data = "<xml></xml>"
         mock__parse_xml_response.return_value = expected_return = "parsed xml"
-        ccs_search = CheckCapacitySummarySearch(
-            age=self.age,
-            age_format=self.age_format,
-            disposition=self.disposition,
-            symptom_group=self.symptom_group,
-            symptom_discriminator_list=self.symptom_discriminator_list,
-            gender=self.gender,
-            role=self.role,
-            postcode=self.postcode,
-            search_environment=self.search_environment,
-            search_date_time=self.search_date_time,
-        )
         mock_post.return_value = MagicMock(status_code=status_code)
         # Act
-        search_details = ccs_search.search()
+        search_details = self.default_ccs_search.search()
         # Assert
         assert search_details == expected_return, "Search details should be parsed xml"
         mock__get_non_prod_username_and_password.assert_called_once()
@@ -156,22 +150,10 @@ class TestCheckCapacitySummarySearch:
         status_code = 500
         mock__get_non_prod_username_and_password.return_value = (username, password)
         mock__build_request_data.return_value = return_data = "<xml></xml>"
-        ccs_search = CheckCapacitySummarySearch(
-            age=self.age,
-            age_format=self.age_format,
-            disposition=self.disposition,
-            symptom_group=self.symptom_group,
-            symptom_discriminator_list=self.symptom_discriminator_list,
-            gender=self.gender,
-            role=self.role,
-            postcode=self.postcode,
-            search_environment=self.search_environment,
-            search_date_time=self.search_date_time,
-        )
         mock_post.return_value = MagicMock(status_code=status_code)
         # Act
         with pytest.raises(CCSAPIResponseError) as exception:  # noqa: PT012
-            response = ccs_search.search()
+            response = self.default_ccs_search.search()
             assert exception == f"CCS Response {status_code}"
             assert response is None, "No expected response"
         # Assert
@@ -195,18 +177,6 @@ class TestCheckCapacitySummarySearch:
     def test_get_non_prod_username_and_password(self, mock_client: MagicMock) -> None:
         """Test the _get_non_prod_username_and_password method."""
         # Arrange
-        ccs_search = CheckCapacitySummarySearch(
-            age=self.age,
-            age_format=self.age_format,
-            disposition=self.disposition,
-            symptom_group=self.symptom_group,
-            symptom_discriminator_list=self.symptom_discriminator_list,
-            gender=self.gender,
-            role=self.role,
-            postcode=self.postcode,
-            search_environment=self.search_environment,
-            search_date_time=self.search_date_time,
-        )
         username_value = "username"
         password_value = "password"
         environ["CCS_SECRET_NAME"] = secret_name = "test"
@@ -216,7 +186,7 @@ class TestCheckCapacitySummarySearch:
             "SecretString": dumps({username_key: username_value, password_key: password_value}),
         }
         # Act
-        username, password = ccs_search._get_non_prod_username_and_password()
+        username, password = self.default_ccs_search._get_non_prod_username_and_password()
         # Assert
         mock_client.return_value.get_secret_value.assert_called_with(SecretId=secret_name)
         assert username == username_value, "Username should be username value"
@@ -228,21 +198,8 @@ class TestCheckCapacitySummarySearch:
 
     def test_get_prod_username_and_password(self) -> None:
         """Test the _get_prod_username_and_password method."""
-        # Arrange
-        ccs_search = CheckCapacitySummarySearch(
-            age=self.age,
-            age_format=self.age_format,
-            disposition=self.disposition,
-            symptom_group=self.symptom_group,
-            symptom_discriminator_list=self.symptom_discriminator_list,
-            gender=self.gender,
-            role=self.role,
-            postcode=self.postcode,
-            search_environment=self.search_environment,
-            search_date_time=self.search_date_time,
-        )
         # Act
-        username, password = ccs_search._get_prod_username_and_password()
+        username, password = self.default_ccs_search._get_prod_username_and_password()
         # Assert
         assert username == self.role, f"Username should be {self.role}"
         assert password == "example-password", "Password should be example-password"
@@ -252,20 +209,8 @@ class TestCheckCapacitySummarySearch:
         # Arrange
         username = "username"
         password = "password"
-        ccs_search = CheckCapacitySummarySearch(
-            age=self.age,
-            age_format=self.age_format,
-            disposition=self.disposition,
-            symptom_group=self.symptom_group,
-            symptom_discriminator_list=self.symptom_discriminator_list,
-            gender=self.gender,
-            role=self.role,
-            postcode=self.postcode,
-            search_environment=self.search_environment,
-            search_date_time=self.search_date_time,
-        )
         # Act
-        request_data = ccs_search._build_request_data(username, password)
+        request_data = self.default_ccs_search._build_request_data(username, password)
         # Assert
         expected_xml = '<?xml version="1.0" ?><soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:web="https://nww.pathwaysdos.nhs.uk/app/api/webservices"><soap:Header><web:serviceVersion>1.5</web:serviceVersion></soap:Header><soap:Body><web:CheckCapacitySummary><web:userInfo><web:username>username</web:username><web:password>password</web:password></web:userInfo><web:c><web:postcode>test</web:postcode><web:age>1</web:age><web:ageFormat>years</web:ageFormat><web:disposition>1</web:disposition><web:symptomGroup>1</web:symptomGroup><web:searchDistance>20</web:searchDistance><web:gender>M</web:gender><web:SearchDateTime>2021-01-01T00:00:00Z</web:SearchDateTime><web:symptomDiscriminatorList><web:int>1</web:int><web:int>2</web:int><web:int>3</web:int></web:symptomDiscriminatorList></web:c></web:CheckCapacitySummary></soap:Body></soap:Envelope>'  # noqa: E501
         assert request_data == expected_xml, "Request data should be as expected"
@@ -273,24 +218,12 @@ class TestCheckCapacitySummarySearch:
     def test_parse_xml_response(self) -> None:
         """Test the _parse_xml_response method."""
         # Arrange
-        ccs_search = CheckCapacitySummarySearch(
-            age=self.age,
-            age_format=self.age_format,
-            disposition=self.disposition,
-            symptom_group=self.symptom_group,
-            symptom_discriminator_list=self.symptom_discriminator_list,
-            gender=self.gender,
-            role=self.role,
-            postcode=self.postcode,
-            search_environment=self.search_environment,
-            search_date_time=self.search_date_time,
-        )
         api_response = parse(  # noqa: S318
             "application/search/tests/ccs_comparison_search/example_ccs_api_response.xml",
         )
         api_response_xml = api_response.toprettyxml()
         # Act
-        response = ccs_search._parse_xml_response(api_response_xml)
+        response = self.default_ccs_search._parse_xml_response(api_response_xml)
         # Assert
         expected_response = [
             Service(
