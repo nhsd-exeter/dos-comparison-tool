6b6d1040d1c47b6d3f88c94349205e630f20fb55
Jack Plowman <jack.plowman1@nhs.net>
2023-05-05 09:35:31 +0100

    Add server access logging for pipeline bucket

--- infrastructure/stacks/development-and-deployment-tools/s3.tf
+++ infrastructure/stacks/development-and-deployment-tools/s3.tf
@@ -56,7 +56,6 @@ module "development_pipeline_log_bucket" {
   version = "3.10.1"
 
   bucket        = var.development_pipeline_log_bucket
-  acl           = "log-delivery-write"
   force_destroy = true
 
   attach_deny_insecure_transport_policy = true
@@ -73,6 +72,32 @@ module "development_pipeline_log_bucket" {
       }
     }
   }
+
+  policy = jsonencode({
+    Statement = [
+      {
+        Action = "kms:*"
+        Effect = "Allow"
+        Principal = {
+          Service = ["s3:PutObject"]
+        }
+        Resource = [
+          "arn:aws:s3:::${var.development_pipeline_log_bucket}/*",
+          "arn:aws:s3:::${var.development_pipeline_log_bucket}/s3/${var.development_pipeline_bucket}/*"
+        ]
+        condition = {
+          test     = "ArnLike"
+          variable = "aws:SourceArn"
+          values   = ["arn:aws:s3:::${var.development_pipeline_bucket}"]
+        }
+        condition = {
+          test     = "StringEquals"
+          variable = "aws:SourceAccount"
+          values   = ["${var.aws_account_id}"]
+        }
+      }
+    ]
+  })
 
   versioning = {
     enabled = true
