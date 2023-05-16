50eb6d29d4962fad33ba8d0f41129808b6c66e0c
Jack Plowman <jack.plowman1@nhs.net>
2023-05-16 17:10:16 +0100

    Further increase test coverage

--- application/search/tests/ccs_comparison_search/test_check_capacity_summary_search.py
+++ application/search/tests/ccs_comparison_search/test_check_capacity_summary_search.py
@@ -235,7 +235,7 @@ class TestCheckCapacitySummarySearch:
         expected_xml = '<?xml version="1.0" ?><soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:web="https://nww.pathwaysdos.nhs.uk/app/api/webservices"><soap:Header><web:serviceVersion>1.5</web:serviceVersion></soap:Header><soap:Body><web:CheckCapacitySummary><web:userInfo><web:username>username</web:username><web:password>password</web:password></web:userInfo><web:c><web:postcode>test</web:postcode><web:age>1</web:age><web:ageFormat>years</web:ageFormat><web:disposition>1</web:disposition><web:symptomGroup>1</web:symptomGroup><web:searchDistance>20</web:searchDistance><web:gender>M</web:gender><web:SearchDateTime>2021-01-01T00:00:00Z</web:SearchDateTime><web:symptomDiscriminatorList><web:int>1</web:int><web:int>2</web:int><web:int>3</web:int></web:symptomDiscriminatorList></web:c></web:CheckCapacitySummary></soap:Body></soap:Envelope>'  # noqa: E501
         assert request_data == expected_xml, "Request data should be as expected"
 
-    def test_parse_xml_response(self) -> None:
+    def test__parse_xml_response(self) -> None:
         """Test the _parse_xml_response method."""
         # Arrange
         api_response = parse(  # noqa: S318
@@ -255,6 +255,18 @@ class TestCheckCapacitySummarySearch:
             ).__dict__,
         ]
         assert expected_response == response, "Service not as expected"
+
+    def test___parse_error_xml_response(self) -> None:
+        """Test the _parse_error_xml_response method."""
+        # Arrange
+        api_response_xml = parse(  # noqa: S318
+            "application/search/tests/ccs_comparison_search/example_ccs_api_error_response.xml",
+        )
+        api_response_xml = api_response_xml.toprettyxml()
+        # Act
+        response = self.default_ccs_search._parse_error_xml_response(api_response_xml)
+        # Assert
+        assert response == ("120", "Parameter Validation: SG not found"), "Response not as expected"
 
 
 def test_compare_search_responses() -> None:
