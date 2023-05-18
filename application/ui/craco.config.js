094a30ea399a1915ad811bd85f07c0283e4d6b77
Jack Plowman <jack.plowman1@nhs.net>
2023-05-18 17:06:16 +0100

    Remove json comments

--- application/ui/craco.config.js
+++ application/ui/craco.config.js
@@ -1,13 +1,10 @@
 module.exports = {
-	// Must be commonjs module
 	style: {
 		sass: {
 			loaderOptions: {
-				// the following scss imports should only include variables and mixins
-				// any included css will be duplicated many times
 				additionalData: `
 					@import "~nhsuk-frontend/packages/core/settings/all";
-						@import "~nhsuk-frontend/packages/core/tools/all";
+					@import "~nhsuk-frontend/packages/core/tools/all";
 				`,
 			},
 		},
